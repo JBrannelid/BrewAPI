e7bfdf5d21da4771529da7d44619c963cc539c99
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-02 10:20:47 +0200

    Update AuthService to use new Auth and User DTO namespaces

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,4 +1,5 @@
-﻿using BrewAPI.DTOs;
+﻿using BrewAPI.DTOs.Auth;
+using BrewAPI.DTOs.User;
 using BrewAPI.Services.IServices;
 
 // Service responsible for authentication operations (registration/login)
