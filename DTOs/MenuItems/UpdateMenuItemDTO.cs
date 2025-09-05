6b1a68cb7b1e02d43e2c68638344e2eca8ccfdbc
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-05 21:37:44 +0200

    Add UpdateMenuItemDTO for menu item update operations

---
+++ DTOs/MenuItems/UpdateMenuItemDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrewAPI.DTOs.MenuItems
+{
+    public class UpdateMenuItemDTO
+    {
+        public string Name { get; set; }
+
+        public string? Category { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Description { get; set; }
+
+        public bool IsPopular { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
