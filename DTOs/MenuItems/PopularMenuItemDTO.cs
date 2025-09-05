2c369cfade3967441c2f8e53e5d1110fa2722a7a
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-05 21:37:44 +0200

    Add PopularMenuItemDTO for displaying popular menu items

---
+++ DTOs/MenuItems/PopularMenuItemDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BrewAPI.DTOs.MenuItems
+{
+    public class PopularMenuItemDTO
+    {
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
