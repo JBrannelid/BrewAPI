96c774c5360b86fa86cb8527d1bab9bb18791292
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-05 21:37:43 +0200

    Refactor MenuItemsController to use specific DTOs and remove category endpoint

--- Controllers/MenuItemsController.cs
+++ Controllers/MenuItemsController.cs
@@ -3,6 +3,11 @@ using BrewAPI.Services.IServices;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+// For now: Authorize policy on AdminsOrManagers. Easy to scale up with different role 
+// MenuItemService interface is injected through the constructor 
+// Each function asynchronously handles CRUD operations
+// TODO: A better global error and logging handeling
+
 namespace BrewAPI.Controllers
 {
     [Route("api/[controller]")]
@@ -16,8 +21,6 @@ namespace BrewAPI.Controllers
             _menuItemService = menuItemService;
         }
 
-        // GET: api/MenuItems
-        // Public endpoint so all customers can see the menu
         [HttpGet]
         public async Task<ActionResult<List<MenuItemDTO>>> GetAllMenuItems()
         {
@@ -25,8 +28,6 @@ namespace BrewAPI.Controllers
             return Ok(menuItems);
         }
 
-        // GET: api/MenuItems/{id}
-        // Public endpoint so a customer can recived a specific menu item by Id 
         [HttpGet("{id}")]
         public async Task<ActionResult<MenuItemDTO>> GetMenuItemById(int id)
         {
@@ -39,43 +40,28 @@ namespace BrewAPI.Controllers
             return Ok(menuItem);
         }
 
-        // GET: api/MenuItems/category/{category}
-        // Public endpoints allow filtering menu items by category
-        [HttpGet("category/{category}")]
-        public async Task<ActionResult<List<MenuItemDTO>>> GetMenuItemsByCategory(string category)
-        {
-            var menuItems = await _menuItemService.GetMenuItemsByCategoryAsync(category);
-            return Ok(menuItems);
-        }
-
-        // GET: api/MenuItems/popular
-        // Public endpoint to fetch and highlighting popular menu items
         [HttpGet("popular")]
-        public async Task<ActionResult<List<MenuItemDTO>>> GetPopularMenuItems()
+        public async Task<ActionResult<List<PopularMenuItemDTO>>> GetPopularMenuItems()
         {
             var popularMenuItems = await _menuItemService.GetPopularMenuItemsAsync();
             return Ok(popularMenuItems);
         }
 
-        // POST: api/MenuItems
-        // Restricted to Admin or Manager since only staff should create menu items
         [HttpPost]
         [Authorize(Policy = "AdminOrManager")]
-        public async Task<ActionResult<int>> CreateMenuItem(MenuItemDTO menuItemDTO)
+        public async Task<ActionResult<int>> CreateMenuItem(CreateMenuItemDTO createMenuItemDTO)
         {
-            var menuItemId = await _menuItemService.CreateMenuItemAsync(menuItemDTO);
+            var menuItemId = await _menuItemService.CreateMenuItemAsync(createMenuItemDTO);
 
             // Returns 201 with Location header
             return CreatedAtAction(nameof(GetMenuItemById), new { id = menuItemId }, menuItemId);
         }
 
-        // PUT: api/MenuItems/{id}
-        // For Admin or Manager to update a menu item (price, img, description, name ect)
         [HttpPut("{id}")]
         [Authorize(Policy = "AdminOrManager")]
-        public async Task<ActionResult> UpdateMenuItem(int id, MenuItemDTO menuItemDTO)
+        public async Task<ActionResult> UpdateMenuItem(int id, UpdateMenuItemDTO updateMenuItemDTO)
         {
-            var result = await _menuItemService.UpdateMenuItemAsync(id, menuItemDTO);
+            var result = await _menuItemService.UpdateMenuItemAsync(id, updateMenuItemDTO);
             if (!result)
             {
                 return NotFound();
