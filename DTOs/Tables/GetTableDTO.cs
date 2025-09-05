dde3a7f158db73164eb9c6849f239eb0c9f17c65
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-05 21:37:45 +0200

    Add GetTableDTO for table retrieval operations

---
+++ DTOs/Tables/GetTableDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BrewAPI.DTOs.Tables
+{
+    public class GetTableDTO
+    {
+        public int TableId { get; set; }
+        public int TableNumber { get; set; }
+        public int Capacity { get; set; }
+    }
+}
