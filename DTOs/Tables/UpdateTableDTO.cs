e10451f170c124306173b8fd25469646f6161653
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-05 21:37:45 +0200

    Add UpdateTableDTO for table update operations

---
+++ DTOs/Tables/UpdateTableDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BrewAPI.DTOs.Tables
+{
+    public class UpdateTableDTO
+    {
+        public int TableNumber { get; set; }
+        public int Capacity { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
