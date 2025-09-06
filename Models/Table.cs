2a7b173e4db12b0d8215dc405cbd843b343e8981
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-06 13:15:08 +0200

    Implement IEntity interface for generic repository support

--- Models/Table.cs
+++ Models/Table.cs
@@ -1,8 +1,9 @@
 ﻿namespace BrewAPI.Models
 {
-    public class Table
+    public class Table : IEntity
     {
         public int PK_TableId { get; set; }
+        public int Id => PK_TableId; 
         public int TableNumber { get; set; }
         public int Capacity { get; set; }
         public bool IsAvailable { get; set; }
