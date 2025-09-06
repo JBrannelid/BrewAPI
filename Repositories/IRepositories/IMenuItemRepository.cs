b4214caa62508d21ee882ffc24d43d0dee2a0def
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-06 13:15:09 +0200

    Update interface to extend IGenericRepository

--- Repositories/IRepositories/IMenuItemRepository.cs
+++ Repositories/IRepositories/IMenuItemRepository.cs
@@ -2,18 +2,8 @@
 
 namespace BrewAPI.Repositories.IRepositories
 {
-    public interface IMenuItemRepository
+    public interface IMenuItemRepository : IGenericRepository<MenuItem>
     {
-        Task<List<MenuItem>> GetAllMenuItemAsync();
-
-        Task<MenuItem?> GetMenuItemByIdAsync(int menuItemId);
-
         Task<List<MenuItem>> GetMenuItemByCategoryAsync(string category);
-
-        Task<int> CreateMenuItemAsync(MenuItem menuItem);
-
-        Task<bool> UpdateMenuItemAsync(MenuItem menuItem);
-
-        Task<bool> DeleteMenuItemAsync(int menuItemId);
     }
 }
