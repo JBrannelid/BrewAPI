d6c1e2f76fe20aa17c39de58aaba9ab5ec043307
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-06 13:15:08 +0200

    Add IEntity interface for generic repository pattern

---
+++ Models/IEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace BrewAPI.Models
+
+// Helper interface for mapping entities with an Id property
+// Keep our repository generic and reusable without dealing with specific PK-Key properties
+
+{
+    public interface IEntity
+    {
+        int Id { get; }
+    }
+}
