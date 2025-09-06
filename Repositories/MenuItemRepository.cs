f2ce41f532e147890721012eb142f703a5bdc8aa
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-06 13:15:09 +0200

    Refactor to extend GenericRepository and remove duplicate CRUD code

--- Repositories/MenuItemRepository.cs
+++ Repositories/MenuItemRepository.cs
@@ -3,72 +3,34 @@ using BrewAPI.Models;
 using BrewAPI.Repositories.IRepositories;
 using Microsoft.EntityFrameworkCore;
 
+// This repository extends the GenericRepository class 
+// CRUD operations are inherited from GenericRepository
+// Override specific methods and add additional methods to handle specific business logic
+
 namespace BrewAPI.Repositories
 {
-    public class MenuItemRepository : IMenuItemRepository
+    public class MenuItemRepository : GenericRepository<MenuItem>, IMenuItemRepository
     {
-        private readonly BrewAPIDbContext _context;
-
-        public MenuItemRepository(BrewAPIDbContext context)
+        public MenuItemRepository(BrewAPIDbContext context) : base(context)
         {
-            _context = context;
         }
 
-        // Adds a new menu item to the Db and returns the primary key (Id) of the created item
-        public async Task<int> CreateMenuItemAsync(MenuItem menuItem)
+        public override async Task<MenuItem?> GetByIdAsync(int id)
         {
-            _context.MenuItems.Add(menuItem);
-            await _context.SaveChangesAsync();
-            return menuItem.PK_MenuItemId;
+            return await _dbSet.FirstOrDefaultAsync(m => m.PK_MenuItemId == id);
         }
 
-        // Deletes a menu item by Id. Returns true if a row was affected. False if no row was affected
-        public async Task<bool> DeleteMenuItemAsync(int menuItemId)
+        public override async Task<bool> DeleteAsync(int id)
         {
-            var rowsAffected = await _context.MenuItems.Where(m => m.PK_MenuItemId == menuItemId).ExecuteDeleteAsync();
-            if (rowsAffected > 0)
-            {
-                return true;
-            }
-            return false;
+            var rowsAffected = await _dbSet.Where(m => m.PK_MenuItemId == id).ExecuteDeleteAsync();
+            return rowsAffected > 0;
         }
 
-        // Retrieves all menu items from the Db
-        public async Task<List<MenuItem>> GetAllMenuItemAsync()
-        {
-            var menuItems = await _context.MenuItems.ToListAsync();
-            return menuItems;
-        }
-
-
-        // Retrieves a single menu item by Id. Returns null (FirstOrDefault) if the item does not exist
-        public async Task<MenuItem?> GetMenuItemByIdAsync(int menuItemId)
-        {
-            var menuItem = await _context.MenuItems
-                .FirstOrDefaultAsync(m => m.PK_MenuItemId == menuItemId);
-            return menuItem;
-        }
-
-        // Retrieve a List of all menu items that belong to a specific category
         public async Task<List<MenuItem>> GetMenuItemByCategoryAsync(string category)
         {
-            var menuItems = await _context.MenuItems
-                // Case insensitive comparison for category
+            return await _dbSet
                 .Where(m => m.Category != null && m.Category.ToLower() == category.ToLower())
                 .ToListAsync();
-            return menuItems;
-        }
-
-        // Updates an existing menu item in the Db. Returns true if a row was affected, false if no changes were made
-        public async Task<bool> UpdateMenuItemAsync(MenuItem menuItem)
-        {
-            _context.MenuItems.Update(menuItem);
-            var result = await _context.SaveChangesAsync();
-            if (result != 0)
-            {
-                return true;
-            }
-            return false;
         }
     }
 }
