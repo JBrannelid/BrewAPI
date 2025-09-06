7cb6bbe55a90c82bc5a434c5f65efe67028f6181
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-06 13:15:09 +0200

    Update interface to extend IGenericRepository

--- Repositories/IRepositories/IUserRepository.cs
+++ Repositories/IRepositories/IUserRepository.cs
@@ -2,18 +2,8 @@
 
 namespace BrewAPI.Repositories.IRepositories
 {
-    public interface IUserRepository
+    public interface IUserRepository : IGenericRepository<User>
     {
-        Task<List<User>> GetAllUsersAsync();
-
-        Task<User?> GetUserByIdAsync(int userId);
-
         Task<User?> GetUserByEmailAsync(string email);
-
-        Task<int> CreateUserAsync(User user);
-
-        Task<bool> UpdateUserAsync(User user);
-
-        Task<bool> DeleteUserAsync(int userId);
     }
 }
