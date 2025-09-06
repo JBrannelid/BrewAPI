66d10344d2c91493050d67111d5551571563b0bd
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-06 13:15:08 +0200

    Add generic repository interface for CRUD operations

---
+++ Repositories/IRepositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using BrewAPI.Models;
+
+namespace BrewAPI.Repositories.IRepositories
+{
+    // Generic repository interface
+    // Collect all DB interaction in one place. Keep the code scalable and DRY
+    // Implement this interface through DI and call the corresponding function
+
+    public interface IGenericRepository<T> where T : class, IEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
