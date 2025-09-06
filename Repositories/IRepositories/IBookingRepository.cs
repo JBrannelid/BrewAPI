250976a76ba510ddab68aafee98fbe37ff3440bb
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-06 13:15:09 +0200

    Update interface to extend IGenericRepository

--- Repositories/IRepositories/IBookingRepository.cs
+++ Repositories/IRepositories/IBookingRepository.cs
@@ -2,20 +2,10 @@
 
 namespace BrewAPI.Repositories.IRepositories
 {
-    public interface IBookingRepository
+    public interface IBookingRepository : IGenericRepository<Booking>
     {
-        Task<List<Booking>> GetAllBookingsAsync();
-
-        Task<Booking?> GetBookingByIdAsync(int bookingId);
-
         Task<List<Booking>> GetBookingsByCustomerIdAsync(int customerId);
 
-        Task<List<Booking>> GetBookingsByTableIdAndDateAsync(int tableId, DateOnly date);
-
-        Task<int> CreateBookingAsync(Booking booking);
-
-        Task<bool> UpdateBookingAsync(Booking booking);
-
-        Task<bool> DeleteBookingAsync(int bookingId);
+        Task<List<Booking>> GetBookingsByTableIdAndDateAsync(int tableId, DateOnly date); 
     }
 }
